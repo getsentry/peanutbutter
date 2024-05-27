f5e73c5d6a978e75901351cd854499e5716bf4f9
Sebastian Zivota <loewenheim@mailbox.org>
2024-05-27 10:09:46 +0200

    Change type to deployment

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -3,7 +3,7 @@
 eval $(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")
 /devinfra/scripts/k8s/k8stunnel
 /devinfra/scripts/k8s/k8s-deploy.py \
-  --type="statefulset" \
+  --type="deployment" \
   --label-selector="${LABEL_SELECTOR}" \
   --image="us-central1-docker.pkg.dev/sentryio/peanutbutter/image:${GO_REVISION_PEANUTBUTTER_REPO}" \
   --container-name="peanutbutter" \
